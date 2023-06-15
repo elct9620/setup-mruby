44a1ef031ae617260bb5fcd02b5ed8e3d3d0fd00
Aotokitsuruya <contact@aotoki.me>
2023-06-15 21:41:21 +0800

    Add tool cache

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,26 @@ import * as path from 'path'
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as io from '@actions/io'
+import * as toolCache from '@actions/tool-cache'
+
+const TOOLNAME = 'mruby'
 
 async function run(): Promise<void> {
   try {
-    const targetVersion: string = core.getInput('mruby-version')
-    const prefix = path.join(os.homedir(), '.rubies', `mruby-${targetVersion}`)
-    core.addPath(`${prefix}/bin`)
+    let prefix
 
-    await installWithRubyBuild(targetVersion, prefix)
+    const targetVersion: string = core.getInput('mruby-version')
+    const cachedPath = toolCache.find(TOOLNAME, targetVersion)
+    if (cachedPath) {
+      prefix = cachedPath
+    } else {
+      prefix = path.join(os.homedir(), '.rubies', `mruby-${targetVersion}`)
+
+      await installWithRubyBuild(targetVersion, prefix)
+      await toolCache.cacheDir(prefix, TOOLNAME, targetVersion)
+    }
+
+    core.addPath(`${prefix}/bin`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
